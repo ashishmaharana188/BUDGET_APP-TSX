ed0c52666dc9c962cbdef54fec64ae6d800d6745
ASHISH WORK_PC <ashishmoharana188@gmail.com>
2025-01-13 15:05:54 +0530

    jan 13 "added test for form inputs"

---
+++ temp/tests.tsx
@@ -0,0 +1,13 @@
+/*import React from "react";
+import { test, expect } from "vitest";
+import { render, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import ExpenseForm from '../../src/components/ExpenseForm'
+
+test('should render invalid form', () => {
+    const wrapper =shallow(<ExpenseForm/>)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    })
+    
+})*/
---
+++ tests/components/ExpenseForm.test.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { test, expect } from "vitest";
+import {
+  render,
+  fireEvent,
+  getByLabelText,
+  getByPlaceholderText,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
+import ExpenseForm from "../../src/components/ExpenseForm";
+import { addExpense } from "./../../src/actions/expenses";
+
+test("should render invalid form", async () => {
+  const { getByPlaceholderText, getByText, asFragment } = render(
+    <MemoryRouter>
+      <ExpenseForm />
+    </MemoryRouter>
+  );
+  const descriptionField = getByPlaceholderText(/Description/i);
+  const amountField = getByPlaceholderText(/Amount/i); // Replace with your actual input label
+
+  const submitButton = getByText(/Add Expense/i);
+  await userEvent.click(submitButton);
+
+  const errorMessage = getByText(/Please provide Description and Amount/i);
+  expect(errorMessage).to.exist;
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("should render description properly with error for amount", async () => {
+  const { getByPlaceholderText, getByText, asFragment } = render(
+    <MemoryRouter>
+      <ExpenseForm />
+    </MemoryRouter>
+  );
+  const descriptionField = getByPlaceholderText(/Description/i);
+  const amountField = getByPlaceholderText(/Amount/i); // Replace with your actual input label
+
+  await userEvent.type(descriptionField, "Testing_1");
+
+  const submitButton = getByText(/Add Expense/i);
+  await userEvent.click(submitButton);
+
+  const errorMessage = getByText(/Please provide Description and Amount/i);
+  expect(errorMessage).to.exist;
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("should render description properly with no error", async () => {
+  const { getByPlaceholderText, getByText, asFragment } = render(
+    <MemoryRouter>
+      <ExpenseForm />
+    </MemoryRouter>
+  );
+  const descriptionField = getByPlaceholderText(/Description/i);
+  const amountField = getByPlaceholderText(/Amount/i); // Replace with your actual input label
+
+  await userEvent.type(descriptionField, "Testing_1");
+  await userEvent.type(amountField, "1000");
+
+  const submitButton = getByText(/Add Expense/i);
+  await userEvent.click(submitButton);
+
+  expect(asFragment()).toMatchSnapshot();
+});
