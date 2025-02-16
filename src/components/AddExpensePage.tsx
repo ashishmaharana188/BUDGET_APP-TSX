0aa4f3b93faf63a60a6e20bc8cf11b9d6ef53cdb
Panipurii <86043589+Ashish-Maharana188@users.noreply.github.com>
2025-02-16 15:23:23 +0530

    test cases for class based AADD and edit expenses(to be made in future)

--- src/components/AddExpensePage.tsx
+++ src/components/AddExpensePage.tsx
@@ -1,9 +1,40 @@
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import { Component } from "react";
 import ExpenseForm from "./ExpenseForm";
-import { addExpense } from '../actions/expenses';
-import { ExpenseFormProps } from '../TS_Interface/GlobalInterface';
-import { Dispatch } from 'redux';
+import { addExpense } from "../actions/expenses";
+import { ExpenseFormProps } from "../TS_Interface/GlobalInterface";
+import { Dispatch } from "redux";
 
+/*class based component*/
+class AddExpensePage extends Component<ExpenseFormProps> {
+  handleSubmit = (expense: any) => {
+    if (this.props.onSubmit) {
+      this.props.onSubmit(expense);
+      console.log("Submitted", expense);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm
+          onSubmit={this.handleSubmit}
+          dispatch={this.props.dispatch}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    onSubmit: (expense: any) => dispatch(addExpense(expense)),
+  };
+};
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
+
+/*
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
 const AddExpensePage = (props: ExpenseFormProps & { dispatch: Dispatch }) => (
     <div>
       <ExpenseForm
@@ -16,5 +47,10 @@ const AddExpensePage = (props: ExpenseFormProps & { dispatch: Dispatch }) => (
       
     </div>
   );
+  const mapDispatchToProps =  (dispatch: any) => {
+    return {
+      onSubmit: (expense: any) => dispatch(addExpense(expense))
+    }
+  }
 
-  export default connect()(AddExpensePage);
+  export default connect(undefined,mapDispatchToProps)(AddExpensePage);*/
