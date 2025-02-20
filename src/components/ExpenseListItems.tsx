24e1c2aa1aba1ff06bee8d923675933e0fe6ffac
Panipurii <86043589+Ashish-Maharana188@users.noreply.github.com>
2025-02-20 21:46:59 +0530

    ...

--- src/components/ExpenseListItems.tsx
+++ src/components/ExpenseListItems.tsx
@@ -1,19 +1,19 @@
-
 import { Link } from "react-router-dom";
 import { expenseReducerIntf } from "../TS_Interface/GlobalInterface";
 
-
-
-
-const ExpenseListItems = ({ id, description, amount, createdDt}: expenseReducerIntf) => (
-    <div>
-        <Link to={`/edit/${id}`}>
-        <h3>{description}</h3>
-        </Link>
-        <p>
-            {amount} - {createdDt}
-        </p>
-        
-    </div>
+const ExpenseListItems = ({
+  id,
+  description,
+  amount,
+  createdDt,
+}: expenseReducerIntf) => (
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {amount} - {createdDt}
+    </p>
+  </div>
 );
- export default ExpenseListItems;
+export default ExpenseListItems;
