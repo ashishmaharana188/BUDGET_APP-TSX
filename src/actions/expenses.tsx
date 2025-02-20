469dcd15b5e9aedd5c8c969095415a28cfee9d9a
Panipurii <86043589+Ashish-Maharana188@users.noreply.github.com>
2025-02-21 01:33:41 +0530

    ...

--- src/actions/expenses.tsx
+++ src/actions/expenses.tsx
@@ -1,28 +1,29 @@
 import { v4 as uuid } from "uuid";
 
 // ADD_EXPENSE
-export const addExpense = ({description= '',
-    note= '',
-    amount= 0,
-    createdDt= 0}= {}
-    ) => ({
-    type: 'ADD_EXPENSE' as const,
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        amount,
-        createdDt
-    }
+export const addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdDt = 0,
+} = {}) => ({
+  type: "ADD_EXPENSE" as const,
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdDt,
+  },
 });
 // REMOVE_EXPENSE
-export const removeExpense = (id:string) => ({
-    type: 'REMOVE_EXPENSE' as const,
-    id
-})
+export const removeExpense = (id: string) => ({
+  type: "REMOVE_EXPENSE" as const,
+  id,
+});
 // EDIT_EXPENSE
 export const editExpense = (id: string, updates: any) => ({
-    type: 'EDIT_EXPENSE' as const,
-    id,
-    updates
-})
+  type: "EDIT_EXPENSE" as const,
+  id,
+  updates,
+});
